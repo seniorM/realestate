c4913245124196508215383f8ebb276557977adb
seniorM <maximum3052@gmail.com>
2019-11-26 22:32:37 +0200

    create posts

---
+++ database/migrations/2019_11_26_190909_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 255);
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->float('price')->nullable();
+            $table->integer('category_id');
+            $table->integer('is_active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
